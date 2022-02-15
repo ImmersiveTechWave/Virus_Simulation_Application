8349d5ab4a32f0f7e58055cd76ec44e2d642b657
Madalina <madalina.spataru14@gmail.com>
2022-02-15 22:37:56 +0200

    feat: create GoToPoint for a given point

--- Licenta-M/Assets/Scripts/Runtime/BehaviorDesignerScripts/Movement/GoToPoint.cs
+++ Licenta-M/Assets/Scripts/Runtime/BehaviorDesignerScripts/Movement/GoToPoint.cs
@@ -1,38 +1,21 @@
-using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
-using System;
+using UnityEngine;
 
 namespace MF
 {
 	public class GoToPoint : BaseAction<ActorController>
 	{
-		private Random random;
-		private bool hasPoints;
-
-		public override void OnAwake()
-		{
-			base.OnAwake();
-			random = new Random();
-		}
+		[RequiredField] public GameObject TargetPosition;
 
 		public override void OnStart()
 		{
 			base.OnStart();
-			var count = Actor.Model.WanderPoints.Count;
-			hasPoints = count == 0 ? false : true;
-			if (hasPoints)
-			{
-				var index = random.Next(0, count);
-				Actor.Movement.AiDestination = Actor.Model.WanderPoints[index].transform.position;
-			}
+			Actor.Movement.AiDestination = TargetPosition.transform.position;
 		}
 
 		public override TaskStatus OnUpdate()
 		{
-			if (!hasPoints)
-			{
-				return TaskStatus.Failure;
-			}
+			Actor.Movement.AiDestination = TargetPosition.transform.position;
 			if (!Actor.Movement.HasAiReachedDestination)
 			{
 				return TaskStatus.Running;
