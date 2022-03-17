99d4c1d9466aa81a1c937328c635e0edf6f4b78c
Madalina <madalina.spataru14@gmail.com>
2022-03-18 00:00:16 +0200

    fix: resolve Merge Request comments

--- Licenta-M/Assets/Scripts/Runtime/Manager/TimeManager.cs
+++ Licenta-M/Assets/Scripts/Runtime/Manager/TimeManager.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 namespace MF
 {
-
 	public class TimeManager : MonoBehaviour
 	{
-		public float StartTime { get; set; }
 		public TimeModel TimeModel { get; set; }
-
 		public TMP_Text TimeText;
+
+		private float startTime;
 
 		private void Awake()
 		{
@@ -19,15 +17,15 @@ namespace MF
 
 		private void Start()
 		{
-			StartTime = Time.time - 10000;
+			startTime = Time.time - 10000;
 		}
 
 		void Update()
 		{
-			TimeModel.Seconds = (int)((Time.time - StartTime) % 60);
-			TimeModel.Minutes = (int)((Time.time - StartTime) / 60) % 60;
-			TimeModel.Hours = (int)((Time.time - StartTime) / 3600) % 60;
-			string newTime = string.Format("{0:0}:{1:00}:{2:00}", TimeModel.Hours, TimeModel.Minutes, TimeModel.Seconds);
+			TimeModel.Seconds = (int)((Time.time - startTime) % 60);
+			TimeModel.Minutes = (int)((Time.time - startTime) / 60) % 60;
+			TimeModel.Hours = (int)((Time.time - startTime) / 3600) % 60;
+			var newTime = string.Format("{0:0}:{1:00}:{2:00}", TimeModel.Hours, TimeModel.Minutes, TimeModel.Seconds);
 			TimeText.text = newTime;
 		}
 	}
