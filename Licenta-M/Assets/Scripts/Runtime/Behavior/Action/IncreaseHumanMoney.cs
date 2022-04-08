fa7f6da7a9612588472b38962f7709be2c5422bb
Madalina <madalina.spataru14@gmail.com>
2022-04-09 00:00:14 +0300

    wip: improve Behavior Tree

--- Licenta-M/Assets/Scripts/Runtime/Behavior/Action/IncreaseHumanMoney.cs
+++ Licenta-M/Assets/Scripts/Runtime/Behavior/Action/IncreaseHumanMoney.cs
@@ -8,6 +8,9 @@ namespace MF
 	public class IncreaseHumanMoney : BaseAction<HumanController>
 	{
 		private float lastTime;
+		[RequiredField] public SharedUtilityScore ScoreToLock1;
+		[RequiredField] public SharedUtilityScore ScoreToLock2;
+		[RequiredField] public SharedUtilityScore ScoreToLock3;
 
 		public override void OnStart()
 		{
@@ -17,11 +20,24 @@ namespace MF
 
 		public override TaskStatus OnUpdate()
 		{
+			ScoreToLock1.Value = new UtilityScore(ScoreToLock1.Value.Score, true);
+			ScoreToLock2.Value = new UtilityScore(ScoreToLock2.Value.Score, true);
+			ScoreToLock3.Value = new UtilityScore(ScoreToLock3.Value.Score, true);
+
 			if (Time.time - lastTime > 0.2f && Actor.Model.Money.Value < 98f)
 			{
 				Actor.Model.Money.Value += 2.0f;
 				lastTime = Time.time;
 			}
+
+			if (Actor.Model.Money.Value > 95f)
+			{
+				ScoreToLock1.Value = new UtilityScore(ScoreToLock1.Value.Score, false);
+				ScoreToLock2.Value = new UtilityScore(ScoreToLock2.Value.Score, false);
+				ScoreToLock3.Value = new UtilityScore(ScoreToLock3.Value.Score, false);
+				return TaskStatus.Success;
+			}
+
 			return TaskStatus.Running;
 		}
 	}
--- Licenta-M/Assets/Scripts/Runtime/Behavior/Movement/GoToPoint.cs
+++ Licenta-M/Assets/Scripts/Runtime/Behavior/Movement/GoToPoint.cs
@@ -6,11 +6,17 @@ namespace MF
 	public class GoToPoint : BaseAction<ActorController>
 	{
 		[RequiredField] public SharedGameObject TargetPosition;
+		[RequiredField] public SharedUtilityScore ScoreToLock1;
+		[RequiredField] public SharedUtilityScore ScoreToLock2;
+		[RequiredField] public SharedUtilityScore ScoreToLock3;
 
 		public override void OnStart()
 		{
 			base.OnStart();
 			Actor.Movement.AiDestination = TargetPosition.Value.transform.position;
+			ScoreToLock1.Value = new UtilityScore(ScoreToLock1.Value.Score, true);
+			ScoreToLock2.Value = new UtilityScore(ScoreToLock2.Value.Score, true);
+			ScoreToLock3.Value = new UtilityScore(ScoreToLock3.Value.Score, true);
 		}
 
 		public override TaskStatus OnUpdate()
@@ -20,6 +26,9 @@ namespace MF
 			{
 				return TaskStatus.Running;
 			}
+			ScoreToLock1.Value = new UtilityScore(ScoreToLock1.Value.Score, false);
+			ScoreToLock2.Value = new UtilityScore(ScoreToLock2.Value.Score, false);
+			ScoreToLock3.Value = new UtilityScore(ScoreToLock3.Value.Score, false);
 			return TaskStatus.Success;
 		}
 	}
