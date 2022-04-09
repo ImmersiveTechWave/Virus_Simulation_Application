567e49a1663e99088290ff1a6c641b1adc42803b
Madalina <madalina.spataru14@gmail.com>
2022-04-09 16:13:56 +0300

    feat: improve Behavior Tree

--- Licenta-M/Assets/Scripts/Runtime/Behavior/Action/IncreaseHumanEnergy.cs
+++ Licenta-M/Assets/Scripts/Runtime/Behavior/Action/IncreaseHumanEnergy.cs
@@ -10,9 +10,6 @@ namespace MF
 	public class IncreaseHumanEnergy : BaseAction<HumanController>
 	{
 		private float lastTime;
-		[RequiredField] public SharedUtilityScore ScoreToLock1;
-		[RequiredField] public SharedUtilityScore ScoreToLock2;
-		[RequiredField] public SharedUtilityScore ScoreToLock3;
 
 		public override void OnStart()
 		{
@@ -22,10 +19,6 @@ namespace MF
 
 		public override TaskStatus OnUpdate()
 		{
-			ScoreToLock1.Value = new UtilityScore(ScoreToLock1.Value.Score, true);
-			ScoreToLock2.Value = new UtilityScore(ScoreToLock2.Value.Score, true);
-			ScoreToLock3.Value = new UtilityScore(ScoreToLock3.Value.Score, true);
-
 			if (Time.time - lastTime > 0.2f && Actor.Model.Energy.Value < 98f)
 			{
 				Actor.Model.Energy.Value += 2.0f;
@@ -34,9 +27,6 @@ namespace MF
 
 			if (Actor.Model.Energy.Value > 95f)
 			{
-				ScoreToLock1.Value = new UtilityScore(ScoreToLock1.Value.Score, false);
-				ScoreToLock2.Value = new UtilityScore(ScoreToLock2.Value.Score, false);
-				ScoreToLock3.Value = new UtilityScore(ScoreToLock3.Value.Score, false);
 				return TaskStatus.Success;
 			}
 
