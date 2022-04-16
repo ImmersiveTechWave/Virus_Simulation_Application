4b175feb91d75b766e9b33b12d9a3d9659e36d69
Madalina <madalina.spataru14@gmail.com>
2022-04-16 15:38:02 +0300

    feat: decrese hunger, money and energy

--- Licenta-M/Assets/Scripts/Runtime/Controllers/HumanController.cs
+++ Licenta-M/Assets/Scripts/Runtime/Controllers/HumanController.cs
@@ -25,7 +25,7 @@ namespace MF
 				}
 				else
 				{
-					Model.Health.Value -= MyModel.HealthDecreaseRate;
+					Model.Health.Value = DecreseHealth(Model.Health.Value);
 				}
 
 				if (Model.Money.Value < 1)
@@ -34,7 +34,7 @@ namespace MF
 				}
 				else
 				{
-					Model.Money.Value -= MyModel.MoneyDecreaseRate;
+					Model.Money.Value = DecreseMoney(Model.Money.Value);
 				}
 
 				if (Model.Energy.Value < 1)
@@ -43,7 +43,7 @@ namespace MF
 				}
 				else
 				{
-					Model.Energy.Value -= MyModel.EnergyDecreaseRate;
+					Model.Energy.Value = DecreseEnergy(Model.Energy.Value);
 				}
 
 				if (Model.Hunger.Value < 1)
@@ -52,9 +52,104 @@ namespace MF
 				}
 				else
 				{
-					Model.Hunger.Value -= MyModel.HungerDecreaseRate;
+					Model.Hunger.Value = DecreseHunger(Model.Hunger.Value);
 				}
 			}
+		}
+
+		public float DecreseEnergy(float modelEnergyValue)
+		{
+			var decreseEnergy = 1f;
+
+			if (Model.CurrentActivity == Activities.IsWorking)
+			{
+				decreseEnergy *= 3f;      // because human spends more time for doing something
+			}
+
+			if (Model.CurrentActivity == Activities.IsEating)
+			{
+				decreseEnergy *= 0.5f;      // because human eats
+			}
+
+			if (Model.CurrentActivity == Activities.IsSlepping)
+			{
+				decreseEnergy *= 0f;      // because human does`t lose energy when he sleeps
+			}
+
+			if (Model.CurrentActivity == Activities.IsMoving)
+			{
+				decreseEnergy *= 2f;      // because human moves
+			}
+
+			Debug.Log("Decrese energy ----------" + decreseEnergy);
+			modelEnergyValue = modelEnergyValue - decreseEnergy;
+			return modelEnergyValue;
+		}
+
+		public float DecreseHunger(float modelHungerValue)
+		{
+			var decreseHunger = 1f;
+
+			if (Model.CurrentActivity == Activities.IsWorking)
+			{
+				decreseHunger *= 4f;      // because human spends more time for doing something and lose more calories
+			}
+
+			if (Model.CurrentActivity == Activities.IsEating)
+			{
+				decreseHunger *= 0f;      // because human eats
+			}
+
+			if (Model.CurrentActivity == Activities.IsSlepping)
+			{
+				decreseHunger *= 0.5f;      // because human sleeps and he consumes some calories
+			}
+
+			if (Model.CurrentActivity == Activities.IsMoving)
+			{
+				decreseHunger *= 1.5f;      // because human moves  he consume calories
+			}
+
+			Debug.Log("Decrese hunger ----------" + decreseHunger);
+			modelHungerValue = modelHungerValue - decreseHunger;
+			return modelHungerValue;
+		}
+
+		public float DecreseMoney(float modelMoneyValue)
+		{
+			var decreseMoney = 1f;
+
+			if (Model.CurrentActivity == Activities.IsWorking)
+			{
+				decreseMoney *= 0f;      // because human earns money, doesn`t lose
+			}
+
+			if (Model.CurrentActivity == Activities.IsEating)
+			{
+				decreseMoney *= 5f;      // because human eats
+			}
+
+			if (Model.CurrentActivity == Activities.IsSlepping)
+			{
+				decreseMoney *= 0f;      // because human sleeps
+			}
+
+			if (Model.CurrentActivity == Activities.IsMoving)
+			{
+				decreseMoney *= 0.5f;      // because human moves 
+			}
+
+			Debug.Log("Decrese money ----------" + decreseMoney);
+			modelMoneyValue = modelMoneyValue - decreseMoney;
+			return modelMoneyValue;
+		}
+
+		public float DecreseHealth(float modelHealthValue)
+		{
+			var decreseHealth = 0f;
+
+			modelHealthValue = modelHealthValue - decreseHealth;
+			return modelHealthValue;
 		}
 	}
 }
