477ab01d06eaccbe3e8d8574ca00ce257ab6fe60
Alexandru <alexandru.franciuc@tractormedia.com>
2022-04-18 01:36:40 +0300

    feat: add day-night skybox

--- Licenta-M/Assets/Scripts/Runtime/Light/LightingManager.cs
+++ Licenta-M/Assets/Scripts/Runtime/Light/LightingManager.cs
@@ -33,7 +33,7 @@ namespace MF
 			{
 				DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
 
-				DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+				DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 110, 170f, 0));
 			}
 
 		}
--- Licenta-M/Assets/Scripts/Runtime/Manager/TimeManager.cs
+++ Licenta-M/Assets/Scripts/Runtime/Manager/TimeManager.cs
@@ -5,6 +5,11 @@ namespace MF
 {
 	public class TimeManager : MonoBehaviour
 	{
+		private const float HOURS = 24f;
+		private const float HALF_HOURS = 12f;
+		private const float MINUTES = 60f;
+		private const float SECONDS = 60f;
+
 		[Range(0f, 24f)] public int StartHour = 8;
 		[Range(0.1f, 5000f)] public float TimeMultipilcator = 500f;
 
@@ -12,10 +17,12 @@ namespace MF
 		public TMP_Text TimeText;
 
 		private float startTime;
+		private SkyboxBlender skyboxBlender;
 
 		private void Awake()
 		{
 			TimeModel = new TimeModel(StartHour, 0, 0);
+			skyboxBlender = GetComponent<SkyboxBlender>();
 		}
 
 		private void Start()
@@ -27,9 +34,21 @@ namespace MF
 		{
 			var currentTime = (Time.time - startTime) * TimeMultipilcator;
 
-			TimeModel.Seconds = (int)(currentTime % 60);
-			TimeModel.Minutes = (int)(currentTime / 60) % 60;
-			TimeModel.Hours = ((int)(currentTime / 3600) + StartHour) % 24;
+			TimeModel.Seconds = (int)(currentTime % SECONDS);
+			TimeModel.Minutes = (int)(currentTime / SECONDS % MINUTES);
+			TimeModel.Hours = (int)(((currentTime / (SECONDS * MINUTES)) + StartHour) % HOURS);
+
+			var skyBoxBlendValue = 0f;
+			if (TimeModel.Hours >= 0 && TimeModel.Hours <= HALF_HOURS)
+			{
+				skyBoxBlendValue = 1 - (TimeModel.Hours * MINUTES + TimeModel.Minutes) / (HALF_HOURS * MINUTES);
+			}
+			if (TimeModel.Hours > HALF_HOURS && TimeModel.Hours <= HOURS)
+			{
+				skyBoxBlendValue = (TimeModel.Hours * MINUTES + TimeModel.Minutes - (HALF_HOURS * MINUTES)) / (HALF_HOURS * MINUTES);
+			}
+
+			skyboxBlender.blend = skyBoxBlendValue;
 
 			var newTime = string.Format("{0:0}:{1:00}:{2:00}", TimeModel.Hours, TimeModel.Minutes, TimeModel.Seconds);
 			if (TimeText != null)
